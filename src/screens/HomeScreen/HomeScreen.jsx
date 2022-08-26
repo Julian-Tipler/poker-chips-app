f26200f0a97054f8aae0437a5c97b7dc0274b54d
Julian <tipler.julian@gmail.com>
2022-08-26 08:22:08 -0500

    basic auth complete

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,16 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { uid } from "uid";
+import { ref, set } from "firebase/database";
 
 export const HomeScreen = () => {
+  const writeToDatabase = () => {
+    const uuid = uid();
+    set(ref(database, `/${uuid}`), {
+      name: "john",
+    });
+  };
+
   return (
     <View>
       <TouchableOpacity onPress={writeToDatabase}>
         <Text>Write Data</Text>
       </TouchableOpacity>
-      <Text>Wallet:</Text>
-      <Text>{player}</Text>
+      <Text></Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,4 +1,3 @@
-import { database } from "../../firebase";
 import {
   KeyboardAvoidingView,
   StyleSheet,
@@ -7,20 +6,54 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useState } from "react";
-import { uid } from "uid";
-import { ref, set } from "firebase/database";
+import { useNavigation } from "@react-navigation/core";
+import React, { useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebase";
 
 export const LoginScreen = () => {
-  const [player, setPlayer] = useState(0);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const writeToDatabase = () => {
-    const uuid = uid();
-    set(ref(database, `/${uuid}`), {
-      name: "john",
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.navigate("Home");
+      }
     });
+  });
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
   };
 
   return (
@@ -37,14 +70,17 @@ export const LoginScreen = () => {
           value={password}
           onChangeText={setPassword}
           style={styles.input}
-          secureTextEntrysdf
+          secureTextEntry
         />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={() => {}} style={styles.button}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
           <Text>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}} style={[styles.button,styles.buttonOutline]}>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
           <Text style={styles.buttonOutlineText}>Register</Text>
         </TouchableOpacity>
       </View>
@@ -55,14 +91,46 @@ export const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
   button: {
-
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
   },
   buttonOutline: {
-
-  }
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
 });
